62f897588dee56d6fd6eca3c064901a7e6739ad7
Mario Alonso <mario.blazter99@gmail.com>
2020-06-17 23:39:47 -0500

    14 Commit, Mejorando el diseño del Listado de los Elementos

--- src/partials/_ListDefault.vue
+++ src/partials/_ListDefault.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>      
+    <div class="mt-3 mt-md-0">      
         <div v-for="e in elementsList" v-bind:key="e.id">
-            <router-link :to=" '/detail/' + e.id ">
-                <b-card :title="e.title">
+            <router-link class="card-link" :to=" '/detail/' + e.id ">
+                <b-card :header="e.title"
+                header-bg-variant="primary"
+                header-text-variant="white">
                     <b-card-text>{{ e.description }}</b-card-text>
                 </b-card>
             </router-link>
@@ -21,15 +23,17 @@ export default {
             elements: []
         };
     },
-    methods: {
-        
-    },
-}
+    methods: {}
+};
 </script>
 
 <style>
-    .box{
-        border: 3px solid #CCC;
-        margin: 5px 0 0 0;
-    }
+a.card-link,
+a.card-link:hover {
+    color: #222;
+    text-decoration: none;
+}
+a.card-link:hover .card-body{
+    background: #F8F9FA;
+}
 </style>
